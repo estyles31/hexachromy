8e6c5c80db406af6cac7b2b2b19cddcae2bbfb0b
Chris Berger <cberger31@gmail.com>
2025-12-06 18:07:20 -0600

    system marker style changed

--- modules/throneworld/frontend/config/systemStyles.ts
+++ modules/throneworld/frontend/config/systemStyles.ts
@@ -4,37 +4,37 @@ export const systemStyles = {
     background: "#e8e8e8",
     border: "#9a9a9a",
     text: "#000000",
-    fog: "#222222"
+    fog: "#4f4f40"
   },
   outer: {
     background: "#d2e3ff",
     border: "#8ca5c7",
     text: "#000000",
-    fog: "#222222"
+    fog: "#4f4f40"
   },
   inner: {
     background: "#ffecb3",
     border: "#d2b775",
     text: "#000000",
-    fog: "#222222"
+    fog: "#4f4f40"
   },
   throneworld: {
     background: "#ffd6d6",
     border: "#c78a8a",
     text: "#000000",
-    fog: "#222222"
+    fog: "#4f4f40"
   },
   homeworld: {
     background: "#fff7dd",
     border: "#d8c79a",
     text: "#000000",
-    fog: "#222222"
+    fog: "#4f4f40"
   },
   default: {
     background: "#dddddd",
     border: "#999999",
     text: "#000000",
-    fog: "#222222"
+    fog: "#4f4f40"
   }
 } as const
 
